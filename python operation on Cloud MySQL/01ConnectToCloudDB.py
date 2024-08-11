bd8ebfa254b7067b9510723712f5da1fa97d6092
Prathamesh Kale <137622594+prath0m@users.noreply.github.com>
2024-08-11 18:27:23 +0530

    Delete python operation on Cloud MySQL directory

--- python operation on Cloud MySQL/01ConnectToCloudDB.py
+++
@@ -1,4 +0,0 @@
-import pymysql
-conn = pymysql.connect(host='b35fhkxul9dakrrvpl8v-mysql.services.clever-cloud.com',user='ujdoqeqcusxjjas5',password='W5zppJbaAzak5mXKEMTO',database='b35fhkxul9dakrrvpl8v')
-print('successfully connected to cloud database')
-conn.close()
--- python operation on Cloud MySQL/08AccountType.py
+++
@@ -1,8 +0,0 @@
-import pymysql
-conn = pymysql.connect(host='b35fhkxul9dakrrvpl8v-mysql.services.clever-cloud.com',user='ujdoqeqcusxjjas5',password='W5zppJbaAzak5mXKEMTO',database='b35fhkxul9dakrrvpl8v')
-curs= conn.cursor()
-typ = input("Enter Account Type: ")
-curs.execute("Select * from accounts where acctype = '%s'"%typ)
-data = curs.fetchall()
-print(data)
-conn.close()
